66d29e1f4347a18739817b50299dd4f11f7930e9
EvanLin3141 <evan.lin.3.141@gmail.com>
2025-11-20 17:37:40 +0000

    NetworkX Introductions

---
+++ algorithm.py
@@ -0,0 +1,22 @@
+
+# @nx._dispatchable(returns_graph=True)
+# def bfs_tree(G, source, reverse=False, depth_limit=None, sort_neighbors=None):    
+# T = nx.DiGraph()
+    # T.add_node(source)
+    # edges_gen = bfs_edges(
+    #     G,
+    #     source,
+    #     reverse=reverse,
+    #     depth_limit=depth_limit,
+    #     sort_neighbors=sort_neighbors,
+    # )
+    # T.add_edges_from(edges_gen)
+    # return T
+
+
+def bfs_tree_Python(G, source, reverse=False, depth_limit=None, sort_neighbors=None):
+    color = []
+    dist = []
+    parent = []
+    for node in G:
+        
